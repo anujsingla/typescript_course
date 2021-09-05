99923275434ca03dbf53e5b2855dfe69b7d146de
Anuj Singla <asingla@redhat.com>
2021-09-05 20:46:17 +0530

    code changes .

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,25 @@
-function add(para1, para2) {
+"use strict";
+function add(para1, para2 = 10, isCalculate = false) {
     const sum = para1 + para2;
-    console.log("hello12 update", sum);
+    console.log("hello12 update", sum, isCalculate);
 }
-add(5, 5);
+add(5);
+const student = {
+    name: "anuj",
+    rollnumber: 7,
+    class: "10th",
+    subject: ["sub1", "sub2"],
+    family: {
+        fatherName: "shyam",
+        motherName: "sunita",
+    },
+};
+console.log(student.family.fatherName);
+console.log(student.subject);
+let arr;
+arr = [1, 2];
+// tuple
+let arrr;
+arrr = ["sss", 12, "anuj"];
+// number, string, boolean, interface object, function
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,42 @@
-function add(para1, para2) {
+function add(para1: number, para2 = 10, isCalculate = false) {
   const sum = para1 + para2;
-  console.log("hello12 update", sum);
+  console.log("hello12 update", sum, isCalculate);
 }
 
-add(5, 5);
+add(5);
+
+interface IFamily {
+  fatherName: string;
+  motherName: string;
+}
+
+interface IStudent {
+  name: string;
+  rollnumber: number;
+  class: string;
+  subject: string[];
+  family: IFamily;
+}
+
+const student: IStudent = {
+  name: "anuj",
+  rollnumber: 7,
+  class: "10th",
+  subject: ["sub1", "sub2"],
+  family: {
+    fatherName: "shyam",
+    motherName: "sunita",
+  },
+};
+
+console.log(student.family.fatherName);
+console.log(student.subject);
+
+let arr: number[];
+arr = [1, 2];
+
+// tuple
+let arrr: [string, number, string];
+arrr = ["sss", 12, "anuj"];
+
+// number, string, boolean, interface object, function
